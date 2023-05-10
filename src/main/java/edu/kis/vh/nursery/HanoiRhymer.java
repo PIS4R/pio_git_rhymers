a243f3c518c39a9a6418b2beb98959dcbd6b7cf8
PIS4R <240777@edu.p.lodz.pl>
2023-05-10 02:27:26 +0200

    2.12 Removed useless setter, one useless getter is still in

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -21,7 +21,4 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
         return totalRejected;
     }
 
-    protected void setTotalRejected(int totalRejected) {
-        this.totalRejected = totalRejected;
-    }
 }
