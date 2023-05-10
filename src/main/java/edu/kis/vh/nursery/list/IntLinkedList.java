ce9b5764d16ac6839cae46b3f964a5b7be6aa2a6
PIS4R <240777@edu.p.lodz.pl>
2023-05-10 12:53:19 +0200

    3.14 Changed access modifier of Node class and moved to IntLinkedList class, and deleted Node class itself

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,5 +38,24 @@ public class IntLinkedList {
     }
 
 }
+class Node {
 
+    protected final int value;
+    private Node next, prev;
+    protected Node(int i) {
+        value = i;
+    }
+    protected Node getPrev() {
+        return prev;
+    }
+    protected void setPrev(Node prev) {
+        this.prev = prev;
+    }
+    protected Node getNext() {
+        return next;
+    }
+    protected void setNext(Node next) {
+        this.next = next;
+    }
+}
 
